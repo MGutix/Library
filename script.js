92b079ca9509019569fb98d41544d1da061c4dc3
MGutix <140160200+MGutix@users.noreply.github.com>
2023-09-25 11:49:12 +0900

    Can toggle isRead on object

--- script.js
+++ script.js
@@ -10,13 +10,22 @@ let isRead;
 
 const myLibrary = [];
 
-class Book {
-  constructor(name, author, pages, isRead) {
+function Book (name, author, pages, isRead) {
+  this.name = name;
+  this.author = author;
+  this.pages = pages;
+  this.isRead = isRead;
 
-    this.name = name;
-    this.author = author;
-    this.pages = pages;
-    this.isRead = isRead;
+  this.toggleIsRead = () => {
+    console.log('hola')
+    if(this.isRead === 'Read'){
+      this.isRead = 'Not Read'
+      //cardIsRead.setAttribute("class", "notRead");
+    } else{
+      this.isRead = 'Read'
+      //cardIsRead.setAttribute("class", "read");
+    }
+    
   }
 }
 
@@ -35,9 +44,6 @@ function addBookToLibrary() {
 
 }
 
-
-
-
 function displayBooks() {
 
 
@@ -55,47 +61,44 @@ function displayBooks() {
   let newBook = new Book(bookName, bookAuthor, bookPages, isRead);
   myLibrary.push(newBook)
 
-  
-
   bookArea.innerHTML = '';
 
-  for (const book of myLibrary) {
+  for (let i = 0; i<myLibrary.length; i++) {
     let newCard = document.createElement('div');
     newCard.setAttribute("class", "bookCard");
-    
 
     let cardBookName = document.createElement('h3');
     cardBookName.setAttribute("class", "cardBookName");
     newCard.appendChild(cardBookName);
-    let contentBookName = document.createTextNode(`"${book.name}"`);
+    let contentBookName = document.createTextNode(`"${myLibrary[i].name}"`);
     cardBookName.appendChild(contentBookName);
 
     let cardBookAuthor = document.createElement('h3');
     cardBookAuthor.setAttribute("class", "cardBookAuthor");
     newCard.appendChild(cardBookAuthor);
-    let contentBookAuthor = document.createTextNode(book.author);
+    let contentBookAuthor = document.createTextNode(myLibrary[i].author);
     cardBookAuthor.appendChild(contentBookAuthor);
 
     let cardBookPages = document.createElement('h3');
     cardBookPages.setAttribute("class", "cardBookPages");
     newCard.appendChild(cardBookPages);
-    let contentBookPage = document.createTextNode(`${book.pages} pages`);
+    let contentBookPage = document.createTextNode(`${myLibrary[i].pages} pages`);
     cardBookPages.appendChild(contentBookPage);
 
     let cardIsRead = document.createElement('button');
-    cardIsRead.setAttribute("id", "cardIsRead");
-    cardIsRead.setAttribute("class", "cardIsRead");
-    if(book.isRead == 'Read'){
-      cardIsRead.setAttribute("class", "read");
+    cardIsRead.setAttribute('data-index', `${i}`);
+    if(myLibrary[i].isRead === 'Read'){
+      cardIsRead.setAttribute('class', 'read');
     } else{
-      cardIsRead.setAttribute("class", "notRead");
+      cardIsRead.setAttribute('class', 'notRead');
     }
+    cardIsRead.classList.add('cardIsRead')
     newCard.appendChild(cardIsRead);
-    let contentIsRead = document.createTextNode(`${book.isRead}`);
+    let contentIsRead = document.createTextNode(`${myLibrary[i].isRead}`);
     cardIsRead.appendChild(contentIsRead);
 
     let cardRemove = document.createElement('button');
-    cardRemove.setAttribute("class", "cardRemove");
+    cardRemove.setAttribute('class', 'cardRemove');
     newCard.appendChild(cardRemove);
     let contentCardRemove = document.createTextNode(`Remove`);
     cardRemove.appendChild(contentCardRemove);
@@ -107,18 +110,9 @@ function displayBooks() {
 addBookBtn.addEventListener('click', addBookToLibrary)
 
 
-/*cardIsRead = document.getElementById('cardIsRead')
-if (cardIsRead) {
-  cardIsRead.addEventListener('click', toggleIsRead)
+cardIsRead = document.getElementsByClassName('cardIsRead');
+for (let i = 0; i < cardIsRead.length; i++) {
+  cardIsRead[i].addEventListener('click', toggleIsRead);
 }
 
-function toggleIsRead() {
-  if(newBook.isRead.value == 'Read'){
-    newBook.isRead = 'Not Read'
-    cardIsRead.setAttribute("class", "notRead");
-  } else{
-    newBook.isRead = 'Read'
-    cardIsRead.setAttribute("class", "read");
-  }
-  
-}*/
+
