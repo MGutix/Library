e158c3411edc8100c9cdec5f01e539ef52854349
MGutix <140160200+MGutix@users.noreply.github.com>
2023-09-25 10:19:10 +0900

    Pre change to i Commit

--- script.js
+++ script.js
@@ -1,103 +1,124 @@
-  let bookArea = document.getElementById('bookArea')
-  let addBookBtn = document.getElementById('addBook')
-  let modal = document.querySelector('[data-modal]')
-  let submit = document.getElementById('submit')
+let bookArea = document.getElementById('bookArea')
+let addBookBtn = document.getElementById('addBook')
+let modal = document.querySelector('[data-modal]')
+let submit = document.getElementById('submit')
 
-  let bookName;
-  let bookAuthor;
-  let bookPages;
-  let isRead;
+let bookName;
+let bookAuthor;
+let bookPages;
+let isRead;
 
-  const myLibrary = [];
+const myLibrary = [];
 
-  function Book(name, author, pages, isRead) {
-    
+class Book {
+  constructor(name, author, pages, isRead) {
+
     this.name = name;
     this.author = author;
     this.pages = pages;
     this.isRead = isRead;
   }
+}
 
-  function addBookToLibrary() {
+function addBookToLibrary() {
 
-    modal.showModal();
+  modal.showModal();
 
-    let inputs = document.querySelectorAll('input') 
-    inputs.forEach(input => {
-      input.value = '';
-    });
-    
-    console.log(myLibrary)
+  let inputs = document.querySelectorAll('input') 
+  inputs.forEach(input => {
+    input.value = '';
+  });
+  
+  console.log(myLibrary)
 
-    submit.addEventListener('click', displayBooks)
+  submit.addEventListener('click', displayBooks)
 
+}
+
+
+
+
+function displayBooks() {
+
+
+  bookName = document.getElementById('bookTitle').value;
+  bookAuthor = document.getElementById('bookAuthor').value;
+  bookPages = document.getElementById('bookPages').value;
+  isRead = document.getElementById('bookIsRead');
+
+  if (isRead.checked){
+    isRead = 'Read'
+  } else {
+    isRead = 'Not Read'
   }
-
+  
+  let newBook = new Book(bookName, bookAuthor, bookPages, isRead);
+  myLibrary.push(newBook)
 
   
 
-  function displayBooks() {
+  bookArea.innerHTML = '';
 
-
-    bookName = document.getElementById('bookTitle').value;
-    bookAuthor = document.getElementById('bookAuthor').value;
-    bookPages = document.getElementById('bookPages').value;
-    isRead = document.getElementById('bookIsRead');
-
-    if (isRead.checked){
-      isRead = 'Read'
-    } else {
-      isRead = 'Not Read'
-    }
-    
-    let newBook = new Book(bookName, bookAuthor, bookPages, isRead);
-    myLibrary.push(newBook)
-
+  for (const book of myLibrary) {
+    let newCard = document.createElement('div');
+    newCard.setAttribute("class", "bookCard");
     
 
-    bookArea.innerHTML = '';
+    let cardBookName = document.createElement('h3');
+    cardBookName.setAttribute("class", "cardBookName");
+    newCard.appendChild(cardBookName);
+    let contentBookName = document.createTextNode(`"${book.name}"`);
+    cardBookName.appendChild(contentBookName);
 
-    for (const book of myLibrary) {
-      let newCard = document.createElement('div');
-      newCard.setAttribute("class", "bookCard");
+    let cardBookAuthor = document.createElement('h3');
+    cardBookAuthor.setAttribute("class", "cardBookAuthor");
+    newCard.appendChild(cardBookAuthor);
+    let contentBookAuthor = document.createTextNode(book.author);
+    cardBookAuthor.appendChild(contentBookAuthor);
 
-      let cardBookName = document.createElement('h3');
-      cardBookName.setAttribute("class", "cardBookName");
-      newCard.appendChild(cardBookName);
-      let contentBookName = document.createTextNode(`"${book.name}"`);
-      cardBookName.appendChild(contentBookName);
+    let cardBookPages = document.createElement('h3');
+    cardBookPages.setAttribute("class", "cardBookPages");
+    newCard.appendChild(cardBookPages);
+    let contentBookPage = document.createTextNode(`${book.pages} pages`);
+    cardBookPages.appendChild(contentBookPage);
 
-      let cardBookAuthor = document.createElement('h3');
-      cardBookAuthor.setAttribute("class", "cardBookAuthor");
-      newCard.appendChild(cardBookAuthor);
-      let contentBookAuthor = document.createTextNode(book.author);
-      cardBookAuthor.appendChild(contentBookAuthor);
-
-      let cardBookPages = document.createElement('h3');
-      cardBookPages.setAttribute("class", "cardBookPages");
-      newCard.appendChild(cardBookPages);
-      let contentBookPage = document.createTextNode(`${book.pages} pages`);
-      cardBookPages.appendChild(contentBookPage);
-
-      let cardIsRead = document.createElement('button');
-      cardIsRead.setAttribute("class", "cardIsRead");
-      if(book.isRead == 'Read'){
-        cardIsRead.setAttribute("class", "read");
-      } else{
-        cardIsRead.setAttribute("class", "notRead");
-      }
-      newCard.appendChild(cardIsRead);
-      let contentIsRead = document.createTextNode(`${book.isRead}`);
-      cardIsRead.appendChild(contentIsRead);
-
-      let cardRemove = document.createElement('button');
-      cardRemove.setAttribute("class", "cardRemove");
-      newCard.appendChild(cardRemove);
-      let contentCardRemove = document.createTextNode(`Remove`);
-      cardRemove.appendChild(contentCardRemove);
-
-      bookArea.appendChild(newCard);
+    let cardIsRead = document.createElement('button');
+    cardIsRead.setAttribute("id", "cardIsRead");
+    cardIsRead.setAttribute("class", "cardIsRead");
+    if(book.isRead == 'Read'){
+      cardIsRead.setAttribute("class", "read");
+    } else{
+      cardIsRead.setAttribute("class", "notRead");
     }
+    newCard.appendChild(cardIsRead);
+    let contentIsRead = document.createTextNode(`${book.isRead}`);
+    cardIsRead.appendChild(contentIsRead);
+
+    let cardRemove = document.createElement('button');
+    cardRemove.setAttribute("class", "cardRemove");
+    newCard.appendChild(cardRemove);
+    let contentCardRemove = document.createTextNode(`Remove`);
+    cardRemove.appendChild(contentCardRemove);
+
+    bookArea.appendChild(newCard);
   }
+}
 
-  addBookBtn.addEventListener('click', addBookToLibrary)
+addBookBtn.addEventListener('click', addBookToLibrary)
+
+
+/*cardIsRead = document.getElementById('cardIsRead')
+if (cardIsRead) {
+  cardIsRead.addEventListener('click', toggleIsRead)
+}
+
+function toggleIsRead() {
+  if(newBook.isRead.value == 'Read'){
+    newBook.isRead = 'Not Read'
+    cardIsRead.setAttribute("class", "notRead");
+  } else{
+    newBook.isRead = 'Read'
+    cardIsRead.setAttribute("class", "read");
+  }
+  
+}*/
